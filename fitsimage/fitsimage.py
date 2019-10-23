10727d548b7c21381a3b64289b0a9e1878a5b62f
Russell Ryan <rryan@duckdunn.stsci.edu>
2019-10-23 15:09:40 -0400

    added rebin method

--- fitsimage/fitsimage.py
+++ fitsimage/fitsimage.py
@@ -1,7 +1,10 @@
 import numpy as np
 from astropy.wcs import WCS
+
 from astropy.io import fits
 from astropy import nddata
+from scipy import ndimage
+#https://docs.scipy.org/doc/scipy-1.3.0/reference/
 
 
 # https://docs.astropy.org/en/stable/nddata/index.html
@@ -26,7 +29,9 @@ class FITSImage(WCS):
 
     def astrom(self):
         pass
-        
+    #https://reproject.readthedocs.io/en/stable/api/reproject.reproject_interp.html#reproject.reproject_interp
+
+
     def extract(self,x0,x1,y0,y1,**kwargs):
         ''' extract a sub array.  equivalent to hextract '''
         
@@ -35,33 +40,96 @@ class FITSImage(WCS):
         position=((y0+y1+1)/2.,(x0+x1+1)/2.)
         img=nddata.extract_array(self.image,shape,position,**kwargs)
 
-        # cut via direct image
-        #x0=np.clip(x0,0,self.shape[1])
-        #x1=np.clip(x1+1,0,self.shape[1])
-        #y0=np.clip(y0,0,self.shape[0])
-        #y1=np.clip(y1+1,0,self.shape[0])
-        #img=self.image[y0:y1,x0:x1]
-
         # update the new header keywords
         hdr=self.header.copy()
-        hdr['NAXIS1']=img.shape[1]+1
-        hdr['NAXIS2']=img.shape[0]+1
+        hdr['NAXIS1']=img.shape[1]
+        hdr['NAXIS2']=img.shape[0]
         hdr['CRPIX1']-=x0
         hdr['CRPIX2']-=y0
-        hdr['LTV1']=-x0
-        hdr['LTV2']=-y0
-        hdr.add_history('Extracted from region (x,y)=[{}:{},{}:{}]'.format(x0,x1,y0,y1))
+        hdr['LTV1']-=x0
+        hdr['LTV2']-=y0
 
         
-        #FITSImage(img,hdr)     
+        # update the history
+        history='Extracted from region (x,y)=[{}:{},{}:{}]'.format(x0,x1,y0,y1)
+        hdr.add_history(history)
+
+        # create the output
+        output=type(self)(img,hdr)
+
+        return output
+
+    def rebin(self,binfact):
+        if np.isscalar(binfact):
+            xbin=binfact
+            ybin=binfact
+        else:
+            xbin=binfact[0]
+            ybin=binfact[1]
+        xratio=1./xbin
+        yratio=1./ybin
+
+        lamb=yratio/xratio
+        pixratio=xratio*yratio
+            
         
-        # output
-        return type(self)(img,hdr)
+        # trim the image first (to deal with edge effects
+        x=xbin*(self.shape[1]//xbin)-1
+        y=ybin*(self.shape[0]//ybin)-1
 
-    def rebin(self):
-        pass
+        sub=self.extract(0,x,0,y)
+        
+        # block average the image
+        img=nddata.block_reduce(sub.image,(xbin,ybin),func=np.average)
+                
+        # get the header
+        hdr=sub.header.copy()
 
+        # update the main the WCS values
+        hdr['CRPIX1']=(hdr['CRPIX1']-1.)*xratio+1.
+        hdr['CRPIX2']=(hdr['CRPIX2']-1.)*yratio+1.
+        if 'CDELT1' in hdr:
+            hdr['CDELT1']=hdr['CDELT1']/xratio
+        if 'CDELT2' in hdr:
+            hdr['CDELT2']=hdr['CDELT2']/yratio
+
+        
+        print("Need to sort out LTVs")
+        #if 'LTV1' in hdr:
+        #    hdr['LTV1']=hdr['LTV1']*xratio
+        #if 'LTV2' in hdr:
+        #    hdr['LTV2']=hdr['LTV2']*yratio
+        hdr['CD1_1']=hdr['CD1_1']/xratio
+        hdr['CD1_2']=hdr['CD1_2']/xratio
+        hdr['CD2_1']=hdr['CD2_1']/yratio
+        hdr['CD2_2']=hdr['CD2_2']/yratio
+        
+        # update the distortion
+        if self.sip is not None:
+            print("HELP, must update SIP")
+        
+
+        # update the BSCALE
+        if not isinstance(img.dtype,np.unsignedinteger):
+            if 'BSCALE' in hdr and hdr['BSCALE']!=1 and hdr['BSCALE']!=0:
+                hdr['BSCALE']=hdr['BSCALE']/pixratio
+            if 'BZERO' in hdr and hdr['BZERO']!=0:
+                hdr['BZERO']=hdr['BZERO']/pixratio
+                
+        
+        # update the history
+        history='Bloack averaged image with factor {}'.format(binfact)
+        hdr.add_history(history)
+
+
+        # create the output
+        output=type(self)(img,hdr)
+
+        return output
+
+        
     def rot(self):
+        #https://docs.scipy.org/doc/scipy-0.16.0/reference/ndimage.html
         pass
 
     def rotate(self):
@@ -77,16 +145,9 @@ class FITSImage(WCS):
         hdul.writeto(filename,overwrite=overwrite)
         hdul.close()
         
-
     
     def ImageHDU(self,**kwargs):
-        #hdr=self.to_header(relax=True)
-        #header=self.header.copy()
-        #for h in hdr:
-        #    header[h]=hdr[h]
-        out=fits.ImageHDU(data=self.image,header=self.header,**kwargs)
-                
-        return out
+        return fits.ImageHDU(data=self.image,header=self.header,**kwargs)
     
 
     
@@ -104,24 +165,43 @@ class FITSImage(WCS):
         elif isinstance(k,tuple):
             self.image[k[0],k[1]]=v
 
-        
-
             
     @property
     def shape(self):
         return self.image.shape
 
+
+    # convenience functions to avoid that dumb 0
+    def xy2ad(self,x,y):
+        return self.all_pix2world(x,y,0)
+
+    def ad2xy(self,a,d):
+        return self.all_world2pix(a,d,0)
+
+    def xy2xy(self,x,y,obj):
+        a,d=self.all_pix2world(x,y,0)
+        return self.all_world2pix(a,d,0)
+      
             
         
 if __name__=='__main__':
-    
-    filename='/Users/rryan/icoi3immq_flt.fits'
-    exten=1
-    x=FITSImage()
-    x.read(filename,exten=exten)
-    sub=x.extract(490,530,490,510)
-    print(sub.wcs.crpix,x.wcs.crpix)
-
-    sub.writefits('t.fits')
 
         
+    filename='/Users/rryan/icoi3immq_flt.fits'
+    exten=1
+    img=FITSImage()
+    img.read(filename,exten=exten)
+    sub=img.extract(490,530,490,510)
+    sub.writefits('sub.fits')
+    binned=sub.rebin(3)
+
+    binned.writefits('python.fits')
+    
+    
+    x=np.array([400,500])
+    y=np.array([400,500])
+    #x=np.array([500])
+    #y=np.array([500])
+    #x=500
+    #y=500
+    
